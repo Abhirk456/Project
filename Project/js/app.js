d6887fc5faf6f798eb2b58fc586067395e0c2750
Abhishek Anand <abhirk247@gmail.com>
2020-10-18 14:21:58 +0530

    first commit

--- Project/js/app.js
+++ Project/js/app.js
@@ -1,1 +1,21 @@
-console.log("it works")
+console.log("now its work")
+
+// IF user add a note, add it to the localStorage
+
+let addBtn = document.getElementById('addBtn');
+addBtn.addEventListener("Click",function(e){
+
+    let addTxt = document.getElementById("addTxt");
+    let notes = localStorage.getItem("notes");
+    if(notes == null){
+        notesObj= [];
+
+    }
+    else{
+        notesObj = JSON.parse(notes);
+     }
+     notesObj.push(addTxt.value);
+     localStorage.setItem("notes", JSON.stringify(notes));
+     addTxt.value="";
+     console.log(notesObj)
+})
